747f3864941f4b11461e02b8c66313f9004afcfd
Piyushlohe <piyushlohe114@gmail.com>
2023-10-25 19:10:21 +0530

    Commit Message

---
+++ userapplication/src/main/java/com/UserApplication/userapplication/authentication/auth/AuthenticationService.java
@@ -0,0 +1,87 @@
+package com.UserApplication.userapplication.authentication.auth;
+
+import com.UserApplication.userapplication.authentication.config.JwtService;
+import com.UserApplication.userapplication.authentication.token.Token;
+import com.UserApplication.userapplication.authentication.token.TokenRepository;
+import com.UserApplication.userapplication.authentication.token.TokenType;
+import com.UserApplication.userapplication.user.Role;
+import com.UserApplication.userapplication.user.User;
+import com.UserApplication.userapplication.userRepository.UserRepository;
+import lombok.RequiredArgsConstructor;
+import org.springframework.beans.factory.annotation.Autowired;
+import org.springframework.security.authentication.AuthenticationManager;
+import org.springframework.security.authentication.UsernamePasswordAuthenticationToken;
+import org.springframework.security.crypto.password.PasswordEncoder;
+import org.springframework.stereotype.Service;
+
+@Service
+@RequiredArgsConstructor
+public class AuthenticationService {
+
+    @Autowired
+    private UserRepository repository;
+    @Autowired
+    private TokenRepository tokenRepository;
+    @Autowired
+    private PasswordEncoder passwordEncoder;
+    @Autowired
+    private JwtService jwtService;
+    @Autowired
+    private AuthenticationManager authenticationManager;
+
+    public AuthenticationResponse register(RegisterRequest request) {
+        var user = User.builder()
+                .firstname(request.getFirstname())
+                .lastname(request.getLastname())
+                .email(request.getEmail())
+                .password(passwordEncoder.encode(request.getPassword()))
+                .role(Role.USER)
+                .build();
+        var savedUser = repository.save(user);
+        var jwtToken = jwtService.generateToken(user);
+        saveUserToken(savedUser, jwtToken);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    public AuthenticationResponse authenticate(AuthenticationRequest request) {
+        authenticationManager.authenticate(
+                new UsernamePasswordAuthenticationToken(
+                        request.getEmail(),
+                        request.getPassword()
+                )
+        );
+        var user = repository.findByEmail(request.getEmail())
+                .orElseThrow();
+        var jwtToken = jwtService.generateToken(user);
+        revokeAllUserTokens(user);
+        saveUserToken(user, jwtToken);
+        return AuthenticationResponse.builder()
+                .token(jwtToken)
+                .build();
+    }
+
+    private void saveUserToken(User user, String jwtToken) {
+        var token = Token.builder()
+                .user(user)
+                .token(jwtToken)
+                .tokenType(TokenType.BEARER)
+                .expired(false)
+                .revoked(false)
+                .build();
+        tokenRepository.save(token);
+    }
+
+    private void revokeAllUserTokens(User user) {
+        var validUserTokens = tokenRepository.findAllValidTokenByUser(user.getId());
+        if (validUserTokens.isEmpty())
+            return;
+        validUserTokens.forEach(token -> {
+            token.setExpired(true);
+            token.setRevoked(true);
+        });
+        tokenRepository.saveAll(validUserTokens);
+    }
+
+}
---
+++ userapplication/src/main/java/com/UserApplication/userapplication/user/Role.java
@@ -0,0 +1,6 @@
+package com.UserApplication.userapplication.user;
+
+public enum Role {
+    USER,
+    ADMIN
+}
